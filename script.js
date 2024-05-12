e0a9ee797b25bad9ba4de28801b48a5bd8aeabac
zaidkhan23x <113798465+zaidkhan23x@users.noreply.github.com>
2024-05-12 22:49:21 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+let showsidebar = ()=>{
+    let sidebar=document.getElementById("sidebar");
+    // sidebar.style.display="block";
+    sidebar.style.left="35%";
+}
+let hidesidebar = ()=>{
+    let sidebar=document.getElementById("sidebar");
+    // sidebar.style.display="none";
+    sidebar.style.left="100%";
+}
